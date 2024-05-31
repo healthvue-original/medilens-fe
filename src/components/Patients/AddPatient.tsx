8d7d4fb7ee2873fb6af95254340782960ada115f
1995navinkumar <navin.kumar@razorpay.com>
2024-05-31 18:25:12 +0530

    loading button

--- src/components/Patients/AddPatient.tsx
+++ src/components/Patients/AddPatient.tsx
@@ -10,16 +10,71 @@ import {
 } from "../ui/card";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 export default function AddPatient(): JSX.Element {
   const [formErrors, setFormErrors] = useState({});
   const onSubmit = (e: FormEvent) => {
     e.preventDefault();
+    console.log("submit");
+
+    setIsLoading(true);
     setFormErrors({
       "patient-name": "Field is required",
     });
   };
+  const [isLoading, setIsLoading] = useState(false);
   return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>Add Patient</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <form onSubmit={onSubmit} className="flex flex-col gap-3">
+          <DialogHeader>
+            <DialogTitle>Add Patient</DialogTitle>
+            <DialogDescription>Add entry for new patients</DialogDescription>
+          </DialogHeader>
+          <div>
+            <Label htmlFor="name">Name</Label>
+            <Input id="patient-name" errorText={formErrors["name"]} />
+          </div>
+          <div>
+            <Label htmlFor="email">Email</Label>
+            <Input id="email" errorText={formErrors["email"]} />
+          </div>
+          <div>
+            <Label htmlFor="age">Age</Label>
+            <Input id="age" errorText={formErrors["age"]} />
+          </div>
+          <div>
+            <Label htmlFor="sex">Sex</Label>
+            <Input id="sex" errorText={formErrors["sex"]} />
+          </div>
+          <div>
+            <Label htmlFor="phone">Phone</Label>
+            <Input id="phone" errorText={formErrors["phone"]} />
+          </div>
+          <DialogFooter>
+            <Button type="submit" loading={isLoading} loadingText="Saving...">
+              Save
+            </Button>
+          </DialogFooter>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+/*
     <div className="flex items-center justify-center h-full">
       <form className=" w-[440px]" onSubmit={onSubmit}>
         <Card>
@@ -55,5 +110,4 @@ export default function AddPatient(): JSX.Element {
         </Card>
       </form>
     </div>
-  );
-}
+*/
--- src/components/Patients/PatientsList.tsx
+++ src/components/Patients/PatientsList.tsx
@@ -1,14 +1,13 @@
 import { NavLink } from "react-router-dom";
 import { DataTableDemo } from "../DataTable";
 import { Button } from "../ui/button";
+import AddPatient from "./AddPatient";
 
 export default function PatientsList(): JSX.Element {
   return (
     <div className="p-16 h-full flex flex-col">
       <div>
-        <Button>
-          <NavLink to={"./add"}>Add Patient</NavLink>
-        </Button>
+        <AddPatient />
       </div>
       <div style={{ height: "calc(100% - 100px)" }} className="flex-1">
         <DataTableDemo />
